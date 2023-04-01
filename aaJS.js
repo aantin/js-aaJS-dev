d1980a70b707d99699b3f09378cc34bb85afc280
Antoine <38399674+aantin@users.noreply.github.com>
2023-04-02 00:24:20 +0900

    feat: add a <Object.getAbsolutePositionOf> method

--- aaJS.js
+++ aaJS.js
@@ -3703,7 +3703,21 @@
         
                 return obj;
             };
-        })()
+        })(),
+        getAbsolutePositionOf:  function (element) {
+            aa.arg.test(element, aa.isElement, "'element'");
+
+            let left    = 0,
+                top     = 0;
+            
+            do {
+                top += element.offsetTop  || 0;
+                left += element.offsetLeft || 0;
+                element = element.offsetParent;
+            } while (element);
+
+            return {top, left};
+        }
     });
     aa.deploy(Object.prototype, {
         // ----------------------------------------------------------------
